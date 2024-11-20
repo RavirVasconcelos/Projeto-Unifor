107a9df004c85e963deb5cafc81ca24f8369a3e7
Ytalo Vasconcelos <ytalovasconcelos01@gmail.com>
2024-11-19 21:09:41 -0300

    Finalizado denovo

--- src/Turma.java
+++ src/Turma.java
@@ -14,6 +14,38 @@ public class Turma {
         this.alunos = new ArrayList<Aluno>(aluno);
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public Disciplina getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public ArrayList<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(ArrayList<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
     public void adicionarAluno(Aluno aluno) {
         alunos.add(aluno);
     }
